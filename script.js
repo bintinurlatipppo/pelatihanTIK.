11a2dc35f193b382f78e345e4c18609d370b5322
bintinurlatipppo <bintinurlatif419@gmail.com>
2025-10-07 12:58:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const tombolSapa = document.getElementById('sapaButton');
 // 2. Tambahkan 'even listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
     // 3. Jalankan kode ini ketika tombol di-klik
-    alert('Halo Terima kasih sudah berkunjung!');
+    alert('Halo! Terima kasih sudah berkunjung!');
 });
