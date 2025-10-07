b3712cf94cc5ead2c03eecc62eb3b9108561f249
bintinurlatipppo <bintinurlatif419@gmail.com>
2025-10-07 12:08:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa=document.getElementByld('sapaButton');
-tombolSapa.addEvenListener('click',function(){
-  alert('HAI GENGGGG!!!!');
+const tombolSapa=document.getElementByld("sapaButton");
+tombolSapa.addEvenListener("click",function(){
+  alert("HAI GENGGGG!!!!");
 });
